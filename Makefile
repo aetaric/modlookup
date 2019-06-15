d522190eed4842549de1b1d7fdcd7cd4514d7036
aetaric <aetaric@gmail.com>
2019-06-14 21:10:28 -0700

    create modlookup-api. This only has one endpoint currently. It will eventually have several.

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: listener
+all: listener api
 clean: 
-	rm modlookup-listener 
+	rm modlookup-listener modlookup-api
 listener: 
 	crystal build src/modlookup-listener.cr
+api:
+	crystal build src/modlookup-api.cr
 install:
 	cp modlookup-listener /usr/local/bin/modlookup-listener
+	cp modlookup-api /usr/local/bin/modlookup-api
---
+++ src/modlookup-api.cr
@@ -0,0 +1,33 @@
+require "mongo"
+require "kemal"
+require "./modlookup.cr"
+
+module Modlookup::API
+
+  config = Modlookup::Config.from_yaml(File.read("./config.yml"))
+
+  if config.mongouser.nil?
+    mongodb = Mongo::Client.new "mongodb://#{config.mongohost}:#{config.mongoport}/#{config.mongodb}"
+  else
+    mongodb = Mongo::Client.new "mongodb://#{config.mongouser}:#{config.mongopass}@#{config.mongohost}:#{config.mongoport}/#{config.mongodb}"
+  end
+
+  db = mongodb[config.mongodb]
+
+  before_get do |env|
+    puts "Setting response content type"
+    env.response.headers.add("Access-Control-Allow-Origin", "*")
+    env.response.content_type = "application/json"
+  end
+
+  get "/api/mod/:nick" do |env|
+    nick = env.params.url["nick"]
+    a = [] of BSON
+    results = db["modstate"].find(BSON.from_json({ "nick": nick }.to_json)).each do |obj|
+      a.push(obj)
+    end
+    a.to_s + "\n"
+  end
+
+  Kemal.run
+end
